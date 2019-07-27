ef066528193ff933a3731f47ffe64ac8e5b1e1e2
Salvatore Santamaria <sam.santamaria@gmail.com>
2019-07-27 15:33:06 -0700

    working on added a new form

--- alpha-blog/config/routes.rb
+++ alpha-blog/config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
  root 'pages#home'
  get 'about', to: 'pages#about'
  
- # creates index and CRUD routes
+  # creates index and CRUD routes
  resources :articles
 
 end
