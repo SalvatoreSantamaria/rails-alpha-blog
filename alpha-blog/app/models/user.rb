9c67c6181dd621a77335d3a13fa60bc6c1f830b9
Salvatore Santamaria <sam.santamaria@gmail.com>
2019-08-31 20:29:33 -0700

    Added bcrypt

--- alpha-blog/app/models/user.rb
+++ alpha-blog/app/models/user.rb
@@ -2,12 +2,12 @@ class User < ActiveRecord::Base
 
   has_many :articles #user is the one side of the one to many, article is plural because it is the many side 
   before_save { self.email = email.downcase }
-
   validates :username, presence: true,
-  uniqueness: { case_sensitive: false },
-  length: { minimum: 3, maximum: 25 }
+    uniqueness: { case_sensitive: false },
+    length: { minimum: 3, maximum: 25 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 105 },
-  uniqueness: { case_sensitive: false },
-  format: { with: VALID_EMAIL_REGEX }
+    uniqueness: { case_sensitive: false },
+    format: { with: VALID_EMAIL_REGEX }
+  has_secure_password
 end
