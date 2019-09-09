39b37fe1b51ce7160044ca8d64da5cfbfaada62d
Salvatore Santamaria <sam.santamaria@gmail.com>
2019-09-08 20:22:12 -0700

    Added show user and gravatar pic ability

--- alpha-blog/app/helpers/application_helper.rb
+++ alpha-blog/app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
-module ApplicationHelper
+# To have a profile image associated with an email account you control, add one to the site en.gravatar.com. This will be the globally recognized avatar (gravatar for short) associated with that email address.
+
+
+module ApplicationHelper #any method that helps with views, put in app helper file
+  def gravatar_for(user, options = { size: 80}) #passing in user object
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.username, class: "img-circle")
+  end
 end
