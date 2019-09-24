a8cb56164e180a57eb3d25e47b4882797345d8f6
Salvatore Santamaria <sam.santamaria@gmail.com>
2019-09-23 20:29:45 -0700

    Retricted actions in the UI

--- alpha-blog/app/controllers/pages_controller.rb
+++ alpha-blog/app/controllers/pages_controller.rb
@@ -1,8 +1,9 @@
 class PagesController < ApplicationController
   def about
   end
-  def home
-  end
 
+  def home
+    redirect_to articles_path if logged_? #if a user is logged in, they get redirected to articles path if root route is selected.
+  end
 
 end
